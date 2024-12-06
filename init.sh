ecd149acc1596a508211e64271bad6e7cdd16c71
CodingManoj <CodingManoj@gmail.com>
2024-12-05 20:04:33 -0500

    Expense Ansible CM

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
-if [ -z "$COMPONENT" -o -z "$ENV" -o -z "$SSH_PASSWORD" ]; then 
+git pull  
+
+if [ -z "$1" -o -z "$2" -o -z "$3" ]; then 
     echo -e "\e[31m Input missing \e[0m"
+    echo -e "Expected Usage : \n\t\t   bash init.sh frontend dev DxxxPwd"
     exit 89
 fi
 
-git pull 
+COMPONENT=$1 
+ENV=$2
+SSH_PASSWORD=$3
+
 ansible-playbook -i ${COMPONENT}-${ENV}.roboshop.internal, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e COMPONENT=${COMPONENT} -e ENV=${ENV} main.yml
 
