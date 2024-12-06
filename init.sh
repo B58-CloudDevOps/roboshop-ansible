b772db5d14829759395b69304f312f06321b8db0
CodingManoj <CodingManoj@gmail.com>
2024-12-05 20:01:36 -0500

    Expense Ansible CM

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 if [ -z "$COMPONENT" -o -z "$ENV" -o -z "$SSH_PASSWORD" ]; then 
-    echo -e "\e[32m Input missing \e[0m"
+    echo -e "\e[31m Input missing \e[0m"
     exit 89
 fi
 
+git pull 
 ansible-playbook -i ${COMPONENT}-${ENV}.roboshop.internal, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e COMPONENT=${COMPONENT} -e ENV=${ENV} main.yml
 
